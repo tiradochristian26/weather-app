7c49904b51ff21cfe5e9bd4abd811dca8655dfc7
Christian tirado <tiradochristian26@gmail.com>
2026-07-30 18:17:38 +0800

    weather app structuring

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,15 @@
 
+import React from "react";
+import { useState } from "react";
+import { GetWeather } from "./services/getWeather";
+
+const App = () => {
+    
+    const [count, setCount] = useState(0)
+
+    return (
+        <> <GetWeather/></>
+    )
+}
+
+export default App;
